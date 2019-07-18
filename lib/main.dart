779d721cfa2efcfed1c03a115d10ccd3ce73ad9f
Elad Cohen <eladc.android@gmail.com>
2019-07-18 17:22:50 +0300

    init branch

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+class Counter extends StatefulWidget {
+  Counter({Key key}) : super(key: key);
+
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+        child: Text('counter: $counter'),
+      ),
+      RaisedButton(onPressed: _incrementCounter)
+    ]);
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      this.counter++;
+    });
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: Container(
-        alignment: Alignment.center,
-        child: Text("Hello World!")),
+        home: Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Hello World!"),
+          Counter()
+        ],
+      ),
     ));
   }
 }
